ea1730d8d023169eb2fcc934517941b20ee8bdd8
Nazihah Islam <140833772+Nazihahaha@users.noreply.github.com>
2025-07-28 15:08:58 +0600

    Expand InputeForm with additional flight fields
    Added new fields to InputeForm including departure time, arrival time, duration, total stops, additional info, and a submit button. This enhances the form to collect more comprehensive flight data for prediction.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 import pandas as pd
-from wtforms import (SelectField, DateField)
+from wtforms import (SelectField, DateField, TimeField, IntegerField, SubmitField)
 from wtforms.validators import DataRequired
 
 train = pd.read_csv("data/train.csv")
@@ -10,7 +10,7 @@ X_data = pd.concat([train, val], axis=0).drop(columns="price")
 
 class InputeForm(FlaskForm):
     airline = SelectField(label="Airline",
-                          choices = X_data.airline.unique().tolist()
+                          choices = X_data.airline.unique().tolist(),
                           validators = [DataRequired()]
                           )
     date_of_journey = DateField(
@@ -18,12 +18,34 @@ class InputeForm(FlaskForm):
         validators=[DataRequired()]
     )
     source = SelectField(
-        label = "Source"
-        choices= X_data.source.unique().tolist()
+        label = "Source",
+        choices= X_data.source.unique().tolist(),
         validators = [DataRequired()]
     )
     destination = SelectField(
-        label = "Destination"
-        choices= X_data.destination.unique().tolist()
+        label = "Destination",
+        choices= X_data.destination.unique().tolist(),
         validators = [DataRequired()]
     )
+    dep_time = TimeField(
+        label="Departure Time",
+        validators=[DataRequired()]
+    )
+    arrival_time = TimeField(
+        label="Arrival Time",
+        validators=[DataRequired()]
+    )
+    duration=IntegerField(
+        label="Duration",
+        validators=[DataRequired()]
+    )
+    total_stope=IntegerField(
+        label="Total Stops",
+        validators=[DataRequired()]
+    )
+    additional_info = SelectField(
+        label="Additional Info",
+        choices=X_data.additional_info.unique().tolist(),
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("Predict")
